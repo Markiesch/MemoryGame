c108247c7538e62da652d14b40017c4762ecb894
Markiesch <marksmeel@gmail.com>
2022-06-25 22:32:01 +0200

    Removed old player storage system

--- store/Players.ts
+++
@@ -1,8 +0,0 @@
-const players = ref<Player[]>([]);
-
-watch(players, () => {
-  localStorage.setItem("players", JSON.stringify(players.value));
-  console.log("saving players....");
-});
-
-export default players;
