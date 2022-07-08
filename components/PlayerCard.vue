306ea30eb259222e512260711105f754d2d2c771
Markiesch <marksmeel@gmail.com>
2022-07-08 20:56:08 +0200

    Added playerCard prop

---
+++ components/PlayerCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { removePlayer } = usePlayers();
+
+defineProps<{ player: Player; overlay: boolean }>();
+</script>
+
+<template>
+  <div v-bind="$attrs" class="player--card" :class="player.avatar.color">
+    <img :src="`/avatars/${player.bot ? 'robot' : player.avatar.src}x256.png`" alt="" />
+    <div v-if="overlay" class="overlay">
+      <button @click="removePlayer(player.id)">
+        <IconTrash />
+      </button>
+      <button @click="player.bot = !player.bot">Toggle bot</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "@/styles/layout/playerCard";
+</style>
